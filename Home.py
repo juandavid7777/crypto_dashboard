ab2743fd064bf18406d436d32429b420ed92b966
juandavid7777 <juandavid7777@gmail.com>
2022-10-10 11:16:11 +0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,39 +21,39 @@ from functions import api_gn_bullet_data, api_tech_bullet_data, api_fg_bullet_da
 
 strl.write("first point")
 
-# df_thresholds = pd.read_csv("thresholds.csv")
+df_thresholds = pd.read_csv("thresholds.csv")
 
-# df = df_thresholds[df_thresholds["type"].isin(["Onchain", "Sentiment", "Technical"])]
+df = df_thresholds[df_thresholds["type"].isin(["Onchain", "Sentiment", "Technical"])]
 
     
-# # Runs functions in loops
-# for i, metric in enumerate(df["metric_name"]):
+# Runs functions in loops
+for i, metric in enumerate(df["metric_name"]):
     
-#     # Defines the source of data to be used
-#     if df.iloc[i]["type"] == "Onchain":
-#         val, prev_val, min_val, max_val = api_gn_bullet_data(metric, df.iloc[i]["api_id"])
-#     elif df.iloc[i]["type"] == "Technical":
-#         val, prev_val, min_val, max_val = api_tech_bullet_data(metric, df.iloc[i]["api_id"])
-#     else:
-#         val, prev_val, min_val, max_val = api_fg_bullet_data(metric, df.iloc[i]["api_id"])
+    # Defines the source of data to be used
+    if df.iloc[i]["type"] == "Onchain":
+        val, prev_val, min_val, max_val = api_gn_bullet_data(metric, df.iloc[i]["api_id"])
+    elif df.iloc[i]["type"] == "Technical":
+        val, prev_val, min_val, max_val = api_tech_bullet_data(metric, df.iloc[i]["api_id"])
+    else:
+        val, prev_val, min_val, max_val = api_fg_bullet_data(metric, df.iloc[i]["api_id"])
     
-#     # Defines ranges to be used
-#     if df.iloc[i]["custom_limit"] == True:
-#         range_vals = [df.iloc[i]["min"], df.iloc[i]["low"], df.iloc[i]["high"], df.iloc[i]["max"]]
+    # Defines ranges to be used
+    if df.iloc[i]["custom_limit"] == True:
+        range_vals = [df.iloc[i]["min"], df.iloc[i]["low"], df.iloc[i]["high"], df.iloc[i]["max"]]
         
-#     else:
-#         range_vals = [min_val, df.iloc[i]["low"], df.iloc[i]["high"], max_val]
+    else:
+        range_vals = [min_val, df.iloc[i]["low"], df.iloc[i]["high"], max_val]
 
-#     # Plots data
-#     fig = bullet_fig_metric(value_in = val,
-#                   previous_val = prev_val,
-#                   title_text = metric,
-#                   ranges = range_vals,
-#                   format_num = df.iloc[i]["format"],
-#                   log_scale = df.iloc[i]["log_scale"]
-#                   )
+    # Plots data
+    fig = bullet_fig_metric(value_in = val,
+                  previous_val = prev_val,
+                  title_text = metric,
+                  ranges = range_vals,
+                  format_num = df.iloc[i]["format"],
+                  log_scale = df.iloc[i]["log_scale"]
+                  )
     
-#     st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
 
     
     
