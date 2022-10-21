ed57a30d1d0ee6c9fcf1a6379072bd32b903593e
juandavid7777 <juandavid7777@gmail.com>
2022-10-21 09:45:10 +0400

    Update 2_⛓_On-Chain_Analysis.py

--- pages/2_⛓_On-Chain_Analysis.py
+++ pages/2_⛓_On-Chain_Analysis.py
@@ -6,13 +6,23 @@ from functions import bounded_metric, colored_metric, api_btc_hist_price, api_gn
 
 strl.set_page_config(layout="wide", page_title="BTC metrics - On Chain", page_icon = "⛓")
 
+# Title
+strl.image("onchain.jpg")
+strl.markdown('<b style="color:darkgoldenrod ; font-size: 30px">On-Chain</b>', unsafe_allow_html=True)
+
+# Summary
+strl.markdown("""---""")
+
+
 df_thresholds = pd.read_csv("thresholds.csv")
 df_meta = df_thresholds[df_thresholds["type"].isin(["Onchain"])]
 
 col_bounded, col_colored= strl.columns(2)
 
 with col_bounded:
+    strl.header("Oscillators thresholds")
     plot_graphs(df_meta, colored = False)
 
 with col_colored:
+    strl.header("Collored distribution")
     plot_graphs(df_meta, colored = True)
