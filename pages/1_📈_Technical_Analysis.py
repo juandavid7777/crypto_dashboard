faee7ae81c4f78320f48591401d3c45781cafc3d
juandavid7777 <juandavid7777@gmail.com>
2022-10-24 14:02:57 +0400

    pages

--- pages/1_📈_Technical_Analysis.py
+++ pages/1_📈_Technical_Analysis.py
@@ -2,7 +2,7 @@ import streamlit as strl
 strl.set_page_config(layout="wide", page_title="BTC metrics - Technical", page_icon = "📈")
 
 # Title
-strl.image("technical.jpg")
+strl.image("technical.jpg", use_column_width = True)
 strl.markdown('<b style="color:darkgoldenrod ; font-size: 44px">Technical</b>', unsafe_allow_html=True)
 
 # Summary
