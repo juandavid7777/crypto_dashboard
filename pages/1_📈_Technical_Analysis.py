5c484451c55abe8a7fd6d2e2f854155130a7f5c9
juandavid7777 <juandavid7777@gmail.com>
2022-11-01 19:52:38 +0400

    Update 1_📈_Technical_Analysis.py

--- pages/1_📈_Technical_Analysis.py
+++ pages/1_📈_Technical_Analysis.py
@@ -1,4 +1,10 @@
+import pandas as pd
+import numpy as np
 import streamlit as strl
+
+from functions import bounded_metric, colored_metric, api_btc_hist_price, api_gn_hist_data, api_fg_hist_data, plot_graphs, api_tech_hist_data
+
+
 strl.set_page_config(layout="wide", page_title="BTC metrics - Technical", page_icon = "📈")
 
 # Title
@@ -7,4 +13,17 @@ strl.image("technical_strip.png", use_column_width = True)
 
 # Summary
 strl.markdown("""---""")
-strl.write("Under construction")
+
+
+df_thresholds = pd.read_csv("thresholds.csv")
+df_meta = df_thresholds[df_thresholds["type"].isin(["Technical"])]
+
+col_bounded, col_colored= strl.columns(2)
+
+with col_bounded:
+    strl.subheader("Oscillators thresholds")
+    plot_graphs(df_meta, colored = False)
+
+with col_colored:
+    strl.subheader("Collored distribution")
+    plot_graphs(df_meta, colored = True)
