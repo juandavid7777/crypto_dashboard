8647d266e3781bd2bb64b26312a4c105c953a7ad
juandavid7777 <juandavid7777@gmail.com>
2023-01-07 16:17:25 +0400

    Create 4_image_Machine_Learning.py

---
+++ pages/4_image_Machine_Learning.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import streamlit as strl
+
+from functions import bounded_metric, colored_metric, api_btc_hist_price, api_gn_hist_data, api_fg_hist_data, plot_graphs
+
+strl.set_page_config(layout="wide", page_title="BTC metrics - Machine Learning", page_icon = "📊")
+
+# Title
+# strl.image("sentiment_strip.png", use_column_width = True)
+# strl.markdown('<b style="color:darkgoldenrod ; font-size: 44px">Sentiment</b>', unsafe_allow_html=True)
+
+# Summary
+strl.markdown("""---""")
+
+df_thresholds = pd.read_csv("thresholds.csv")
+df_meta = df_thresholds[df_thresholds["type"].isin(["Sentiment"])]
